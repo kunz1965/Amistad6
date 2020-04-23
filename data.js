68ec6b09661cd654932079cf8c3aba505f873252
kunz1965 <63274896+kunz1965@users.noreply.github.com>
2020-04-23 15:53:26 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,541 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "Fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.24042800920329555,
+        "pitch": -0.02664828241028161,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3920564380818323,
+          "pitch": 0.19611948466576834,
+          "rotation": 0,
+          "target": "1-ambiente1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.24042800920329555,
+          "pitch": -0.02664828241028161,
+          "title": "Bodega en renta",
+          "text": "Calle Amistad #6, Tlalnepantla"
+        }
+      ]
+    },
+    {
+      "id": "1-ambiente1",
+      "name": "Ambiente1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.400187676253811,
+        "pitch": -0.1647348367181074,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03607970483126621,
+          "pitch": -0.006532250129801298,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        },
+        {
+          "yaw": 1.0908085024220018,
+          "pitch": -0.20818083996159586,
+          "rotation": 0,
+          "target": "9-tapanco1"
+        },
+        {
+          "yaw": 0.8649595079377548,
+          "pitch": 0.07381151648447926,
+          "rotation": 0,
+          "target": "3-ambiente3"
+        },
+        {
+          "yaw": 3.031671030383719,
+          "pitch": 0.18478544938901464,
+          "rotation": 0,
+          "target": "0-fachada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5928025158071861,
+          "pitch": -0.2075687983424661,
+          "title": "1900 m2, acceso para trailer",
+          "text": "Baños amplios, espacio comedor u oficinas en tapanco, oficina planta baja y espacio para vigilancia/ velador.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-ambiente2",
+      "name": "Ambiente2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -2.9957217771791704,
+        "pitch": 0.14776777794596008,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.722179675286174,
+          "pitch": 0.11943773163317495,
+          "rotation": 0,
+          "target": "1-ambiente1"
+        },
+        {
+          "yaw": 2.9674278265551832,
+          "pitch": 0.11502560264485595,
+          "rotation": 0,
+          "target": "3-ambiente3"
+        },
+        {
+          "yaw": 2.6029546469183202,
+          "pitch": 0.09205471258492537,
+          "rotation": 0,
+          "target": "4-baoshombres1"
+        },
+        {
+          "yaw": 2.7090321936711614,
+          "pitch": 0.10929544130955193,
+          "rotation": 0,
+          "target": "6-baosmujeres1"
+        },
+        {
+          "yaw": 2.883685551689152,
+          "pitch": -0.09741050303777321,
+          "rotation": 0,
+          "target": "9-tapanco1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ambiente3",
+      "name": "Ambiente3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20937765803660113,
+          "pitch": 0.12719107948783126,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        },
+        {
+          "yaw": 2.712122993999307,
+          "pitch": 0.22547157915302307,
+          "rotation": 0,
+          "target": "8-oficinapb"
+        },
+        {
+          "yaw": -1.6323045534480691,
+          "pitch": 0.16290200973720736,
+          "rotation": 0,
+          "target": "1-ambiente1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9240925765842203,
+          "pitch": -0.0723667738408853,
+          "title": "Espacio para vigilancia/velador",
+          "text": "Con acceso individual"
+        }
+      ]
+    },
+    {
+      "id": "4-baoshombres1",
+      "name": "BañosHombres1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.04667245234230677,
+        "pitch": 0.31303665038691264,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.43089866532228,
+          "pitch": 0.10381635012767276,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        },
+        {
+          "yaw": -0.7390459208719005,
+          "pitch": 0.5228063016605269,
+          "rotation": 0,
+          "target": "5-baoshombres2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-baoshombres2",
+      "name": "BañosHombres2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.32947542001932106,
+        "pitch": 0.27406368264811753,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8228289258418,
+          "pitch": 0.47777671084638307,
+          "rotation": 0,
+          "target": "4-baoshombres1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-baosmujeres1",
+      "name": "BañosMujeres1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -3.552713678800501e-15,
+        "pitch": 0.49904965241073285,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.781468677648613,
+          "pitch": 0.11044148934299969,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        },
+        {
+          "yaw": -0.24926484144457817,
+          "pitch": 0.48307595790610414,
+          "rotation": 0,
+          "target": "7-baosmujeres2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-baosmujeres2",
+      "name": "BañosMujeres2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.19596813486075249,
+        "pitch": 0.22754803103462606,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9966944684980525,
+          "pitch": 0.37238215007732833,
+          "rotation": 0,
+          "target": "6-baosmujeres1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-oficinapb",
+      "name": "OficinaPB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.15850439125255633,
+        "pitch": 0.35127281359009643,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1250054539287735,
+          "pitch": 0.665821684273773,
+          "rotation": 0,
+          "target": "3-ambiente3"
+        },
+        {
+          "yaw": 3.031216876438041,
+          "pitch": 0.1550655422175069,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0090188873524983,
+          "pitch": 0.03563863426503033,
+          "title": "Oficina Planta Baja",
+          "text": "Con baño"
+        }
+      ]
+    },
+    {
+      "id": "9-tapanco1",
+      "name": "Tapanco1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.5770202302855001,
+        "pitch": 0.237407215193425,
+        "fov": 1.43416210789885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0014387414430654388,
+          "pitch": 0.27293029125982926,
+          "rotation": 0,
+          "target": "2-ambiente2"
+        },
+        {
+          "yaw": -1.476244090603755,
+          "pitch": 0.3689943143637997,
+          "rotation": 0,
+          "target": "1-ambiente1"
+        },
+        {
+          "yaw": 1.3750180302242523,
+          "pitch": 0.33882600679063124,
+          "rotation": 0,
+          "target": "10-tapanco2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-tapanco2",
+      "name": "Tapanco2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.131882334993014,
+          "pitch": 0.18487668491285625,
+          "rotation": 0,
+          "target": "9-tapanco1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
